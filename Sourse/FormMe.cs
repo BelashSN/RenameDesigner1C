01399f602e13873e79bebf8eb922b111d19898b9
BelashSN <bsnimpy@gmail.com>
2025-04-28 20:12:41 +0300

    Formating C# code

--- Sourse/FormMe.cs
+++ Sourse/FormMe.cs
@@ -11,15 +11,23 @@ namespace _1CRenameDesigner
 {
     public partial class FormMe : Form
     {
+        #region Global variables
         // ======================================= разрешение закрытия окна
         private static bool ClosePossible = false;
+
         // ======================================= импорт функции обращения к чужому окну
         [DllImport("user32.dll")] public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
-        // ======================================= инициализация формы
+        #endregion
+
+        #region Сonstructor
+        // ======================================= Конструктор
         public FormMe()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Events of form
         // ======================================= при первом открытии формы (значения по-умолчанию)
         private void FormMe_Load(object sender, EventArgs e)
         {
@@ -27,8 +35,8 @@ namespace _1CRenameDesigner
             chkSkip.Checked = false;    // - не показывать пропуски по умолчанию
             chkRename.Checked = true;   // - показывать замены по умолчанию
             chkErrors.Checked = true;   // - показывать ошибки по умолчанию
-
         }
+        
         // ======================================= скопировать текст в клипбоард по двойному клику на поле
         private void TextResult_MouseDoubleClick(object sender, MouseEventArgs e)
         {
@@ -38,6 +46,7 @@ namespace _1CRenameDesigner
             lblClickForCopy.ForeColor = Color.Red;
             TimerCopyLog.Enabled = true;
         }
+        
         // ======================================= стереть надпись о копировании лога в буфео обмена 
         private void TimerCopyLog_Tick(object sender, EventArgs e)
         {
@@ -45,6 +54,7 @@ namespace _1CRenameDesigner
             lblClickForCopy.ForeColor = Color.Blue;
             TimerCopyLog.Enabled = false;
         }
+       
         // ======================================= меню - показать скрытое окно
         private void toolStripMenuItemShow_Click(object sender, EventArgs e)
         {
@@ -52,6 +62,7 @@ namespace _1CRenameDesigner
             this.ShowInTaskbar = true;                  // - показать кнопку панели задач
             this.WindowState = FormWindowState.Normal;  // - восстановить положение формы
         }
+        
         // ======================================= меню - скрыть окно из виду
         private void toolStripMenuItemClose_Click(object sender, EventArgs e)
         {
@@ -60,27 +71,31 @@ namespace _1CRenameDesigner
             this.Visible = false;                           // - скрыть форму из видимости
             TextResult.Text = "";                           // - очистить текст лога формы
         }
+        
         // ======================================= меню - закрыть приложение
         private void toolStripMenuItemExit_Click(object sender, EventArgs e)
         {
             ClosePossible = true;   // - разрешить закрытие формы
             Application.Exit();     // - выход из приложения
         }
+        
         // ======================================= при закрытии - скрыть окно из виду
         private void FormMe_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (!ClosePossible) e.Cancel = true;        // - если закрытие запрещено - отменим действие
             toolStripMenuItemClose_Click(null, null);   // - вызов меню скрытия формы
         }
+        
         // ======================================= вызов меню при клике левой кнопкой на иконке в трее
         private void NotifyIconMe_MouseClick(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)  // - если кнопка мыши левая
-            {
-                MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);    // - получить метод правой кнопки
+            if (e.Button == MouseButtons.Left) { // - если кнопка мыши левая
+                // - получить метод правой кнопки
+                MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);    
                 mi.Invoke(NotifyIconMe, null);  // - вызов метода правой кнопки - меню иконки в тре
             }
         }
+        
         // ======================================= вывод результата в окно сообщений
         private void ShowTextResult(String strResult)
         {
@@ -91,6 +106,7 @@ namespace _1CRenameDesigner
                 + "    ==========================\r\n"
                 + strResult + "\r\n" + TextResult.Text;         // - текст лога под заголовком
         }
+       
         // ======================================= поиск и переименование окон конфигуратора 1с по таймеру
         private void TimerMe_Tick(object sender, EventArgs e)
         {
@@ -101,8 +117,7 @@ namespace _1CRenameDesigner
             ManagementObjectSearcher searcher
                 = new ManagementObjectSearcher("root\\CIMV2", "SELECT CommandLine,ProcessId FROM Win32_Process WHERE CommandLine LIKE '%DESIGNER%' AND Name LIKE '%1cv8%'");
             //-------------------   // - перебор полученных результатов запроса
-            foreach (ManagementObject queryObj in searcher.Get())
-            {
+            foreach (ManagementObject queryObj in searcher.Get()) {
                 String CaptionNew = "";                                 // - новый заголовок окна 1С
                 String CaptionAdd = "║";                                // - имя базы из командной строки по умолчанию
                 String topResult = "------------  ║  ------------";     // - шапка текущего резултата в лог
@@ -116,30 +131,31 @@ namespace _1CRenameDesigner
                 IntPtr Main1cHWND = Process1c.MainWindowHandle;                                          // - запомним дескриптор окна процесса
                 //-------------------
                 MassCommand = strCommand.Replace(" /", "║/").Split('║');                 // - разбить командную строку на отдельные параметры по разделителям
-                for (int i = 0; i < MassCommand.Length; i++)                             // - перебираем полученные строки
-                {
-                    if (MassCommand[i].IndexOf("/F") > 0) { CaptionAdd = MassCommand[i].Replace("/F", "") + "  "; break; }              // - имя базы для файловой версии
-                    else if (MassCommand[i].IndexOf("/S") > 0) { CaptionAdd = MassCommand[i].Replace("/S", "") + "  "; break; }         // - имя базы для серверной версии
-                    else if (MassCommand[i].IndexOf("/IBName") >= 0) { CaptionAdd = MassCommand[i].Replace("/IBName", "").Replace("\"", "") + "  "; break; }    // - имя базы из стартера 1С
+                for (int i = 0; i < MassCommand.Length; i++) {                           // - перебираем полученные строки
+                    if (MassCommand[i].IndexOf("/F") > 0) { 
+                        CaptionAdd = MassCommand[i].Replace("/F", "") + "  "; break; }              // - имя базы для файловой версии
+                    else if (MassCommand[i].IndexOf("/S") > 0) { 
+                        CaptionAdd = MassCommand[i].Replace("/S", "") + "  "; break; }         // - имя базы для серверной версии
+                    else if (MassCommand[i].IndexOf("/IBName") >= 0) { 
+                        CaptionAdd = MassCommand[i].Replace("/IBName", "").Replace("\"", "") + "  "; break; }    // - имя базы из стартера 1С
                 }
                 //-------------------
-                if (CaptionAdd == "║")                                  // --- Не нашли ключ для имени базы - запишем ошибку в лог (если разрешено)
-                {
-                    if (chkErrors.Checked) strResult += topResult.Replace("║", "Не найден ключ (/F,/S/IBName)") + strBottom + "\r\n- " + CaptionOld + "\r\n";
-                }
-                else if (CaptionOld.IndexOf(CaptionAdd) >= 0)           // --- существующий заголовок окна уже содержит имя базы - пропуск в лог (если разрешено)  
-                {
-                    if (chkSkip.Checked) strResult += topResult.Replace("║", "Пропуск (переименовано ранее)") + strBottom + "\r\n- " + CaptionOld + "\r\n";
-                } 
-                else                                                    // --- Создаем новый заголовок 1С: имя базы + старый заголовок
-                {   CaptionNew += CaptionAdd + CaptionOld;
+                if (CaptionAdd == "║") {                                // --- Не нашли ключ для имени базы - запишем ошибку в лог (если разрешено)
+                    if (chkErrors.Checked) strResult += 
+                            topResult.Replace("║", "Не найден ключ (/F,/S/IBName)")  + strBottom + "\r\n- " + CaptionOld + "\r\n";  }
+                else if (CaptionOld.IndexOf(CaptionAdd) >= 0) {         // --- существующий заголовок окна уже содержит имя базы - пропуск в лог (если разрешено)  
+                    if (chkSkip.Checked) strResult += 
+                            topResult.Replace("║", "Пропуск (переименовано ранее)") + strBottom + "\r\n- " + CaptionOld + "\r\n"; }
+                else {                                                   // --- Создаем новый заголовок 1С: имя базы + старый заголовок
+                    CaptionNew += CaptionAdd + CaptionOld;
                     SendMessage(Main1cHWND, 0x000C, 0, CaptionNew);     // --- пошлем сообщение окну о смене заголовка
                     //-------------------                               // --- сохраним строку о замене в лог  (если разрешено) 
-                    if (chkRename.Checked) strResult += topResult.Replace("║", "Замена") + strBottom + "\r\n-  С: " + CaptionOld + "\r\n- На: " + CaptionNew + "\r\n";
-                }
+                    if (chkRename.Checked) strResult += 
+                            topResult.Replace("║", "Замена") + strBottom + "\r\n-  С: " + CaptionOld + "\r\n- На: " + CaptionNew + "\r\n";  }
             }
             //-------------------
             if (this.Visible) ShowTextResult(strResult);                // --- если форма не скрыта - вызовем вывод лога на форму
         }
+        #endregion
     }
 }
