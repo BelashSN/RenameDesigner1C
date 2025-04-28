8cfa146b801e5d80f38b2a5a09b1c7e403da5530
BelashSN <bsnimpy@gmail.com>
2025-04-28 18:40:16 +0300

    Добавьте файлы проекта.

---
+++ Sourse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _1CRenameDesigner
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMe());
+        }
+    }
+}
